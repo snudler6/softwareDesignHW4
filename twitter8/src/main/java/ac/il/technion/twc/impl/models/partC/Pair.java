90539d57c70b771a373983b66b6017b70ad56deb
stlevy <harry1992@gmail.com>
2014-06-20 20:40:25 +0300

    now prints in order

--- twitter8/src/main/java/ac/il/technion/twc/impl/models/partC/Pair.java
+++ twitter8/src/main/java/ac/il/technion/twc/impl/models/partC/Pair.java
@@ -2,45 +2,46 @@ package ac.il.technion.twc.impl.models.partC;
 
 import java.io.Serializable;
 
-
 public class Pair<T> implements Serializable {
-    /**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1239202504731787857L;
 	public final T first;
-    public final T second;
+	public final T second;
 
-    public Pair(T first, T second) {
-    	super();
-    	this.first = first;
-    	this.second = second;
-    }
+	public Pair(T first, T second) {
+		super();
+		this.first = first;
+		this.second = second;
+	}
 
-    public int hashCode() {
-    	int hashFirst = first != null ? first.hashCode() : 0;
-    	int hashSecond = second != null ? second.hashCode() : 0;
+	public int hashCode() {
+		int hashFirst = first != null ? first.hashCode() : 0;
+		int hashSecond = second != null ? second.hashCode() : 0;
 
-    	return (hashFirst + hashSecond) * hashSecond + hashFirst;
-    }
+		return (hashFirst + hashSecond) * hashSecond + hashFirst;
+	}
 
-    public boolean equals(Object other) {
-    	if (other instanceof Pair) {
-    		Pair otherPair = (Pair) other;
-    		return 
-    		((  this.first == otherPair.first ||
-    			( this.first != null && otherPair.first != null &&
-    			  this.first.equals(otherPair.first))) &&
-    		 (	this.second == otherPair.second ||
-    			( this.second != null && otherPair.second != null &&
-    			  this.second.equals(otherPair.second))) );
-    	}
+	public boolean equals(Object other) {
+		if (other instanceof Pair) {
+			Pair otherPair = (Pair) other;
+			return ((this.first == otherPair.first || (this.first != null
+					&& otherPair.first != null && this.first
+						.equals(otherPair.first))) && (this.second == otherPair.second || (this.second != null
+					&& otherPair.second != null && this.second
+						.equals(otherPair.second))));
+		}
 
-    	return false;
-    }
+		return false;
+	}
 
-    public String toString()
-    { 
-           return first + ", " + second; 
-    }
- }
+	public String toString() {
+		String firstString = first.toString();
+		String secondString = second.toString();
+		if(firstString.compareTo(secondString) < 0)
+			return first + ", " + second;
+		else
+			return second + ", " + first;
+	}
+}
