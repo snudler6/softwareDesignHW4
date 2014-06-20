afd52307f8f84f144b9ae175ae5ad29e21bb2521
Shachar <Shachar@ShacharAsus>
2014-06-20 17:24:39 +0300

    created pair class

---
+++ twitter8/src/main/java/ac/il/technion/twc/impl/models/partC/Pair.java
@@ -0,0 +1,48 @@
+package ac.il.technion.twc.impl.models.partC;
+
+public class Pair<T> {
+    private final T first;
+    private final T second;
+
+    public Pair(T first, T second) {
+    	super();
+    	this.first = first;
+    	this.second = second;
+    }
+
+    public int hashCode() {
+    	int hashFirst = first != null ? first.hashCode() : 0;
+    	int hashSecond = second != null ? second.hashCode() : 0;
+
+    	return (hashFirst + hashSecond) * hashSecond + hashFirst;
+    }
+
+    public boolean equals(Object other) {
+    	if (other instanceof Pair) {
+    		Pair otherPair = (Pair) other;
+    		return 
+    		((  this.first == otherPair.first ||
+    			( this.first != null && otherPair.first != null &&
+    			  this.first.equals(otherPair.first))) &&
+    		 (	this.second == otherPair.second ||
+    			( this.second != null && otherPair.second != null &&
+    			  this.second.equals(otherPair.second))) );
+    	}
+
+    	return false;
+    }
+
+    public String toString()
+    { 
+           return "(" + first + ", " + second + ")"; 
+    }
+
+    public T getFirst() {
+    	return first;
+    }
+
+    public T getSecond() {
+    	return second;
+    }
+
+}
