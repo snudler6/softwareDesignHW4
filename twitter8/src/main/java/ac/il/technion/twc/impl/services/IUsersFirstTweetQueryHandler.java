d06dba2239cdf0112c1eec8f8f3c6e3bbe6af350
Shachar <Shachar@ShacharAsus>
2014-06-20 14:09:44 +0300

    created another quary holder of users first tweet

---
+++ twitter8/src/main/java/ac/il/technion/twc/impl/services/IUsersFirstTweetQueryHandler.java
@@ -0,0 +1,20 @@
+package ac.il.technion.twc.impl.services;
+
+import ac.il.technion.twc.api.TweetId;
+import ac.il.technion.twc.api.interfaces.IQueryHandler;
+ 
+/**
+ * manages the popularity of hashtags
+ */
+public interface IUsersFirstTweetQueryHandler extends IQueryHandler
+{
+
+	/**
+	 * Returns the popularity of the hashtag
+	 * 
+	 * @param hashtag the hashtag to get the popularity of
+	 * @return 0 if the hashtag does not exist. otherwise, the number of
+	 *         retweets to roottweets that contains the given hashtag 
+	 */
+	public TweetId getUsersFirstTweetId(String userId);
+}
