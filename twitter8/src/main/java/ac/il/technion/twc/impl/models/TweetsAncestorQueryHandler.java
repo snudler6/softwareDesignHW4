3e20622f645809d580dcc45f085c85801dbcdbd1
Shachar <Shachar@ShacharAsus>
2014-06-20 12:31:52 +0300

    created new quary. created the interface of the quary. created test for the quary

---
+++ twitter8/src/main/java/ac/il/technion/twc/impl/models/TweetsAncestorQueryHandler.java
@@ -0,0 +1,46 @@
+package ac.il.technion.twc.impl.models;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import ac.il.technion.twc.api.TweetId;
+import ac.il.technion.twc.api.interfaces.ITweetsRepository;
+import ac.il.technion.twc.api.models.Tweet;
+import ac.il.technion.twc.impl.services.ITweetsAncestorQueryHandler;
+
+public class TweetsAncestorQueryHandler implements ITweetsAncestorQueryHandler
+{
+	private static final long serialVersionUID = -5818986663944504124L;
+	
+	private Map<TweetId,TweetId> tweetsAncestor;
+	
+	public TweetsAncestorQueryHandler()
+	{
+		this.tweetsAncestor = new HashMap<TweetId, TweetId>();
+	}
+
+	/* (non-Javadoc)
+	 * @see ac.il.technion.twc.api.interfaces.IQueryHandler#onTweetsAdded(java.util.List, ac.il.technion.twc.api.interfaces.ITweetsRepository)
+	 */
+	public void onTweetsAdded(List<Tweet> tweets, ITweetsRepository tweetsRepository)
+	{
+		for (Tweet tweet : tweets)
+		{
+			tweetsAncestor.put(tweet.getId(), tweetsRepository.getRootTweet(tweet).getId());
+		}
+	}
+	
+	/* (non-Javadoc)
+	 * @see ac.il.technion.twc.api.interfaces.IQueryHandler#onRootTweetsDataChanged(java.util.List)
+	 */
+	@Override
+	public void onRootTweetsDataChanged(List<Tweet> changedRootTweets)
+	{
+	}
+
+	@Override
+	public TweetId getAncestor(TweetId id) {
+		return tweetsAncestor.get(id);
+	}
+}
---
+++ twitter8/src/test/java/ac/il/technion/twc/unitTests/TweetsAncestorQueryHandlerTest.java
@@ -0,0 +1,53 @@
+package ac.il.technion.twc.unitTests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import ac.il.technion.twc.api.TweetId;
+import ac.il.technion.twc.api.interfaces.ITweetsRepository;
+import ac.il.technion.twc.api.models.Retweet;
+import ac.il.technion.twc.api.models.RootTweet;
+import ac.il.technion.twc.impl.models.TweetsAncestorQueryHandler;
+
+import com.googlecode.totallylazy.Lists;
+
+public class TweetsAncestorQueryHandlerTest {
+	
+	private static final TweetId ROOT_ID = new TweetId("root");
+	private static final TweetId SON1_ID = new TweetId("son1");
+	private static final TweetId SON2_ID = new TweetId("son2");
+	TweetsAncestorQueryHandler qh = new TweetsAncestorQueryHandler();
+
+	@Before
+	public void setUp() throws Exception {
+		RootTweet root = Mockito.mock(RootTweet.class);
+		Mockito.when(root.getId()).thenReturn(ROOT_ID);
+		
+		Retweet son1 = Mockito.mock(Retweet.class);
+		Mockito.when(son1.getId()).thenReturn(SON1_ID);
+		Mockito.when(son1.getOriginalTweetId()).thenReturn(ROOT_ID);
+		
+		Retweet son2 = Mockito.mock(Retweet.class);
+		Mockito.when(son2.getId()).thenReturn(SON2_ID);
+		Mockito.when(son2.getOriginalTweetId()).thenReturn(SON1_ID);
+		
+		ITweetsRepository repository = Mockito.mock(ITweetsRepository.class);
+		Mockito.when(repository.getRootTweet(root)).thenReturn(root);
+		Mockito.when(repository.getRootTweet(son1)).thenReturn(root);
+		Mockito.when(repository.getRootTweet(son2)).thenReturn(root);
+		
+		
+		qh.onTweetsAdded(Lists.list(root, son1, son2), repository);
+	}
+
+	@Test
+	public void test() {
+		assertEquals(qh.getAncestor(ROOT_ID), ROOT_ID);
+		assertEquals(qh.getAncestor(SON1_ID), ROOT_ID);
+		assertEquals(qh.getAncestor(SON2_ID), ROOT_ID);
+	}
+
+}
